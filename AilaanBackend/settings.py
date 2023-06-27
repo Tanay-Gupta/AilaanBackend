6dd6339df99cb3e0255cb28f8bab3731b08dab50
Tanay <tanaygupta811@gmail.com>
2023-06-27 17:17:56 +0530

    host change

--- AilaanBackend/settings.py
+++ AilaanBackend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ouj6*1_(&h+$&*v2p+ne7+vospwnse#w0cci^8is+d#gq9ekv1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
